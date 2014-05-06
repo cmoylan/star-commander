8f524bd641c8528674748166e9ce20972552dc9c
Chris Moylan <chris@chrismoylan.com>
2014-05-06 18:28:18 -0500

    trying to use sdl

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 #OBJS specifies which files to compile as part of the project
-OBJS = src/Character.cpp src/Level.cpp src/Util.cpp src/main.cpp
+#OBJS = src/Character.cpp src/Level.cpp src/main.cpp
+OBJS = src/main.cpp
 
 #CC specifies which compiler we're using
 CC = g++
@@ -8,7 +9,7 @@ CC = g++
 INCLUDE_PATHS = -I/usr/local/include -I/opt/X11/include
 
 #LIBRARY_PATHS specifies the additional library paths we'll need
-#LIBRARY_PATHS = -L/usr/local/lib -I/opt/X11/lib
+LIBRARY_PATHS = -L/usr/local/lib -I/opt/X11/lib
 
 #COMPILER_FLAGS specifies the additional compilation options we're using
 # g 	 - compile with debug information
@@ -16,17 +17,21 @@ INCLUDE_PATHS = -I/usr/local/include -I/opt/X11/include
 # Wextra - show extra compiler warninres
 COMPILER_FLAGS = -g -Wall -Wextra $(OPTFLAGS)
 
-
 #LINKER_FLAGS specifies the libraries we're linking against
-LINKER_FLAGS = -lGL -lGLU -lglut
-#LINKER_FLAGS = -framework OpenGL -lGLUT `pkg-config ILU --libs`
+#LINKER_FLAGS = -lGL -lGLU -lglut
+#SDL_STUFF = `/usr/local/bin/sdl2-config --libs --cflags`
+SDL_STUFF = `pkg-config --libs --cflags sdl2`
+LINKER_FLAGS = -framework OpenGL $(SDL_STUFF)
 
 #OBJ_NAME specifies the name of our exectuable
 OBJ_NAME = engine
 
 #This is the target that compiles our executable
 all : clean $(OBJS)
-	$(CC) $(OBJS) $(INCLUDE_PATHS) $(LIBRARY_PATHS) $(COMPILER_FLAGS) $(LINKER_FLAGS) -o $(OBJ_NAME)
+	$(CC) $(OBJS) \
+	$(INCLUDE_PATHS) $(LIBRARY_PATHS) \
+	$(COMPILER_FLAGS) $(LINKER_FLAGS) \
+	-o $(OBJ_NAME)
 
 clean:
 	rm -f $(OBJ_NAME)
--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,69 @@
-#include "Util.h"
+#include <SDL.h>
+#include <SDL_opengl.h>
 
-void runMainLoop(int val);
+
+//void runMainLoop(int val);
 
 int
 main(int argc, char *args[])
 {
-  // intialize FreeGLUT
-  glutInit(&argc, args);
+  // setup
+  SDL_Init(SDL_INIT_VIDEO);
 
-  // create OpenGL 2.1 context
-  glutInitContextVersion(2, 1);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 
-  // create double-buffered window
-  glutInitDisplayMode(GLUT_DOUBLE);
-  glutInitWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-  glutCreateWindow("OpenGL");
+  SDL_Window *window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
 
-  // do post window/context creation initialization
-  if (!initGL()) {
-    // TODO: better debugging
-    printf("Unable to initialize graphics library!\n");
-    return 1;
+  SDL_GLContext context = SDL_GL_CreateContext(window);
+
+  // main loop
+  SDL_Event windowEvent;
+  while(true) {
+    if (SDL_PollEvent(&windowEvent)) {
+      if (windowEvent.type == SDL_QUIT) break;
+    }
+
+    SDL_GL_SwapWindow(window);
   }
 
-  // set keyboard handler
-  glutKeyboardFunc(handleKeys);
-
-  // set rendering function
-  glutDisplayFunc(render);
-
-  // set main loop
-  glutTimerFunc(1000 / SCREEN_FPS, runMainLoop, 0);
-
-  // start main loop
-  glutMainLoop();
+  // teardown
+  SDL_GL_DeleteContext(context);
+  SDL_Quit();
 
   return 0;
+
+  // --------------------------------------------------------------
+  // intialize FreeGLUT
+  //glutInit(&argc, args);
+  //
+  //// create OpenGL 2.1 context
+  //glutInitContextVersion(2, 1);
+  //
+  //// create double-buffered window
+  //glutInitDisplayMode(GLUT_DOUBLE);
+  //glutInitWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+  //glutCreateWindow("OpenGL");
+  //
+  //// do post window/context creation initialization
+  //if (!initGL()) {
+  //  // TODO: better debugging
+  //  printf("Unable to initialize graphics library!\n");
+  //  return 1;
+  //}
+  //
+  //// set keyboard handler
+  //glutKeyboardFunc(handleKeys);
+  //
+  //// set rendering function
+  //glutDisplayFunc(render);
+  //
+  //// set main loop
+  //glutTimerFunc(1000 / SCREEN_FPS, runMainLoop, 0);
+  //
+  //// start main loop
+  //glutMainLoop();
+  //
+  //return 0;
 }
