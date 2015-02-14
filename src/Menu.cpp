56d62edf29d8a5af1209bf695b5f7edfd9ca9624
Chris Moylan <chris@chrismoylan.com>
2015-02-14 12:41:28 -0600

    giving up

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -10,9 +10,30 @@ Menu::Menu()
 
     initGL();
 
+    // --- font stuff
+      // Create a pixmap font from a TrueType file.
+    font = new FTPixmapFont("/home/chris/Downloads/freesans/sfd/FreeMonoBold.ttf");
+    //FTGLTextureFont("/home/chris/Downloads/freesans/sfd/FreeMonoBold.ttf");
+
+    // If something went wrong, bail out.
+    if(font->Error())
+      printf("error with the font\n");
+
+    // Set the font size and render a small text.
+    if (!font->FaceSize(72))
+      printf("error with face size!\n");
+    // --- end font stuff
+
     // test texture for now
     glGenTextures(1, &tex);
-    Util::loadTexture(tex, "res/test-bg.png");
+    //Util::loadTexture(tex, "res/test-bg.png");
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    
+    //FTPoint* texture = font->Render("fuck yea");
+    //texture->
+    
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 
@@ -89,20 +110,51 @@ Menu::render()
     // calculate width of menu, and center it
     // may need a texture loader
 
-    glUseProgram(shaderProgram);
-    glBindVertexArray(vao);
-    glBindTexture(GL_TEXTURE_2D, tex);
+    //glUseProgram(shaderProgram);
+    // glBindVertexArray(vao);
+    // glBindTexture(GL_TEXTURE_2D, tex);
+    //
+    // glm::mat4 trans;
+    // trans = glm::translate(trans,
+    //                        glm::vec3(-(SCALE_X * (float) leftEdge),
+    //                                  (SCALE_Y * (float) 1),
+    //                                  1.0f));
+    // glUniformMatrix4fv(uniTrans, 1, GL_FALSE, glm::value_ptr(trans));
+    glOrtho();
+    glUseProgram(0);
+    glColor4f(1.0, 1.0, 1.0, 0.9);
+    //font->Render("Hello World!", 0, 0);
+    glColor3f(1.0, 1.0, 1.0); /* White inside */
+    font->Render("Hello", -1, FTPoint(), FTPoint());
+//     glEnable(GL_BLEND);
+//     //font->Render("Hello World!");
+//     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+     Util::resetGlState();
+    
+    //glLoadIdentity();
+    //glColor3f( 1.0, 0.0, 0.0);
+//    glPushAttrib(GL_ALL_ATTRIB_BITS);
+//    glEnable(GL_BLEND);
+//    glDisable(GL_LIGHTING);
+//    glDisable(GL_DEPTH_TEST);
+//    glColor3f(0.0, 0.0, 1.0);
+//    font->Render("Hello World!");
+//    //glDrawElements(GL_STATIC_DRAW, 1, GL_UNSIGNED_INT, 0);
+//    glPopAttrib();
+     //glUseProgram(0);
 
-    glm::mat4 trans;
-    trans = glm::translate(trans,
-                           glm::vec3(-(SCALE_X * (float) leftEdge),
-                                     (SCALE_Y * (float) 1),
-                                     1.0f));
-    glUniformMatrix4fv(uniTrans, 1, GL_FALSE, glm::value_ptr(trans));
+    //float sc = 0.035;
 
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    //glPushMatrix();
+    //glPushAttrib(GL_ALL_ATTRIB_BITS);
+    //glTranslatef(0.0, 0.0, 10.0);
+    //glRotatef(0.0, 1.0, 0.0, 0.0);
+    //glScalef(1.0, 0.75, 1.0);
+     //    font->Render("Hello World");
+    //glPopMatrix();
+    //glPopAttrib();
 
-    Util::resetGlState();
+    
 }
 
 
--- src/Menu.h
+++ src/Menu.h
@@ -4,6 +4,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <FTGL/ftgl.h>
 
 #include "Constants.h"
 #include "OpenGL.h"
@@ -20,7 +21,8 @@ public:
     menuItems currentSelection;
     int itemWidth;
     int itemHeight;
-
+    FTPixmapFont* font;
+  
     Menu();
     ~Menu();
 
